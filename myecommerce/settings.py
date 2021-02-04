2e6539fe8b240d6402f6d245a3d2e753d5774edd
Syed Furkhan Mehdi <53236773+furkhan26@users.noreply.github.com>
2021-02-05 00:06:23 +0530

    Update settings.py

--- myecommerce/settings.py
+++ myecommerce/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecomjanuary',
         'USER': 'root',
-        'PASSWORD':'Mehdi2606f6$',
+        'PASSWORD':'your password',
         'PORT':3306,
         'HOST':'127.0.0.1'
     }
